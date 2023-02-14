dcaf5450b7528302a264caa137a7dc332c3f7bf7
Paulo Deyvid <88635207+PDEYVID@users.noreply.github.com>
2023-02-14 12:55:25 -0400

    Update Operadores.java

--- JavaBasico/src/Operadores.java
+++ JavaBasico/src/Operadores.java
@@ -4,5 +4,20 @@
 public class Operadores {
 
     public static void main(String[] args) {
+
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1 && (7>4)){
+            System.out.println("as duas são verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições e verdadeira");
+        }
+
+        System.out.println("fim");
+
+
     }
 }
